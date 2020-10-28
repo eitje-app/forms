5efe3e316c9ebbbbc402246a3f6b032e37d35d7a
Abel van Hoek <abelvanhoek@gmail.com>
2020-10-28 15:23:12 +0100

    finally worked it out with babel and lcass functions

---
+++ src/form.js
@@ -0,0 +1,18 @@
+import _ from 'lodash';
+import React, {Component, useState, Fragment, PropTypes, useRef, createRef} from 'react';
+
+class Party {
+
+  myAwesomeness = () => {
+    console.log('hi')
+  }
+
+}
+
+const a = () => {
+  console.log("b")
+}
+
+a()
+
+
