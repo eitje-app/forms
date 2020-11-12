7200fb3e1fd734ef882a97d7de1f4ed49d19def8
Abel van Hoek <abelvanhoek@gmail.com>
2020-11-12 16:15:01 +0100

    allow ref to be passed, strange setup but lets accept for now

--- src/form_creator.js
+++ src/form_creator.js
@@ -3,17 +3,33 @@ import React, {Component, useState, Fragment, PropTypes, useRef, createRef} from
 import Form from './form'
 import {rules as _rules, messages as _messages} from './validation'
 
-const FormCreator = ( {rules = {}, messages = {}, defaultProps = {} } = {} ) => props => {
+// const FormCreator = ( {rules = {}, messages = {}, defaultProps = {} } = {} ) => React.forwardRef((props, ref) => {
+//   const allRules = {field: {..._rules.field, ...rules.field}, name: {..._rules.name, ...rules.name} }
 
-  const allRules = {field: {..._rules.field, ...rules.field}, name: {..._rules.name, ...rules.name} }
+//   const allMessages = {field: {..._messages.field, ...messages.field}, name: {..._messages.name, ...messages.name} }
 
-  const allMessages = {field: {..._messages.field, ...messages.field}, name: {..._messages.name, ...messages.name} }
+//   return <Form rules={allRules} ref={ref} messages={allMessages} {...defaultProps} {...props} />
+// }
 
-  return <Form rules={allRules} messages={allMessages} {...defaultProps} {...props} />
+
+
+
+function makeForm( {rules = {}, messages = {}, defaultProps = {} } = {} ) {
+   const allRules = {field: {..._rules.field, ...rules.field}, name: {..._rules.name, ...rules.name} }
+   const allMessages = {field: {..._messages.field, ...messages.field}, name: {..._messages.name, ...messages.name} }
+
+   class MyForm extends Component {
+
+    render() {
+      const {forwardRef, ...rest} = this.props
+      return <Form ref={forwardRef} {...rest}/>
+    }
+   }
+
+   return React.forwardRef((props, ref) => {
+    return <MyForm forwardRef={ref} rules={allRules} messages={allMessages} {...props} />
+  })
+
 }
 
-export default FormCreator
-
-
-
-
+export default makeForm
