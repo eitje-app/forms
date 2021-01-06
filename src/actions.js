c98b056e57451dc252a318fe0435ece503f1ed79
Abel van Hoek <abelvanhoek@gmail.com>
2021-01-06 12:38:33 +0100

    add ability to have a layered input, meaning that it has a left container (label & element) and a right container (submitButton)

--- src/actions.js
+++ src/actions.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import useFormField from './use_form_field'
-import {Button} from './base'
+import {Button, t} from './base'
 
 
 export const makeField = (Comp, {withLabel = true, withError = true} = {}) => props => {
-  const {containerStyle = {}, field, Container = 'div', isTouched, disabledStyle = {opacity: 0.2}, 
-        containerProps = {}, submitStrategy, submitForm } = props
-  
+  let {containerStyle = {}, field, Container = 'div', isTouched, disabledStyle = {opacity: 0.2}, 
+        containerProps = {}, submitStrategy, submitForm, isLayered = submitStrategy === 'inlineButton',
+        LeftContainer = Fragment, RightContainer = Fragment, leftContainerProps = {}, rightContainerProps = {} } = props
+
+
+ if(isLayered) {
+    LeftContainer = RightContainer = "div"
+    leftContainerProps = {className: 'form-container-left'}
+    rightContainerProps = {className: 'form-container-right'}
+ }
+
   const isButtonSubmit = submitStrategy === 'inlineButton'
-  
+    
   const prupz = useFormField(props)
   const {label, error, disabled} = prupz
 
@@ -23,9 +31,18 @@ export const makeField = (Comp, {withLabel = true, withError = true} = {}) => pr
 
   return (
       <Container style={style} {...containerProps} >
+
+        <LeftContainer {...leftContainerProps}>
         {withLabel && label && label}
           <Comp innerClass={classNames} {...props} {...prupz}/>
-          {isButtonSubmit && isTouched && <Button onClick={() => submitForm({field})}> submit gap </Button> }
+        </LeftContainer>
+
+        <RightContainer {...rightContainerProps}>
+          {isButtonSubmit && isTouched && 
+            <Button onClick={() => submitForm({field})}> {t("submit")} </Button> 
+          }
+          
+          </RightContainer>
         {withError && error && error}
       </Container>
 
