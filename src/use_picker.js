29688559c5a1934874c359c15df61f3f8b5a2f48
Abel van Hoek <abelvanhoek@gmail.com>
2021-06-22 16:03:43 +0200

    use prettier for formatting, use makeLabel also if it's not a simple map, seems safe due to the default.

--- src/use_picker.js
+++ src/use_picker.js
@@ -2,30 +2,53 @@ import React from 'react'
 import _ from 'lodash'
 import {t, isScoped} from './base'
 
-
 export const simpleMap = (item, buildLabel) => {
-  const label = buildLabel ? buildLabel(item) :  t(isScoped ? `form.dropdown.${item}` : item, item)
-  return {label, key: item, value: item }
+  const label = buildLabel ? buildLabel(item) : makeLabel(item)
+  return {label, key: item, value: item}
 }
 
-const usePicker = ({items, noSort, value, multiple, defaultTitle = '-', formData, buildLabel, modifyItems = items => items, labelField="name", valueField="id", sortField=labelField}) => {
-  let pickerItems = items;
-  if(!noSort && sortField) pickerItems = _.sortBy(pickerItems, i => i[sortField]);
+const makeLabel = (label) => {
+  if (!label) return
+  if (isScoped) return t(`form.dropdown.${label}`, label)
+  return t(label, label)
+}
 
-  pickerItems = pickerItems.map(t => !t[labelField] && !_.isObject(t) ? simpleMap(t, buildLabel) : 
-                                      ({label: (buildLabel ? buildLabel(t[labelField], t) : t[labelField]) || "", key: String(t[valueField]), value: t[valueField], ...t }) ) 
+const usePicker = ({
+  items,
+  noSort,
+  value,
+  multiple,
+  defaultTitle = '-',
+  formData,
+  buildLabel,
+  modifyItems = (items) => items,
+  labelField = 'name',
+  valueField = 'id',
+  sortField = labelField,
+}) => {
+  let pickerItems = items
+  if (!noSort && sortField) pickerItems = _.sortBy(pickerItems, (i) => i[sortField])
+
+  pickerItems = pickerItems.map((t) =>
+    !t[labelField] && !_.isObject(t)
+      ? simpleMap(t, buildLabel)
+      : {
+          label: (buildLabel ? buildLabel(t[labelField], t) : makeLabel(t[labelField])) || '',
+          key: String(t[valueField]),
+          value: t[valueField],
+          ...t,
+        },
+  )
 
   pickerItems = modifyItems(pickerItems, formData) || []
 
-  const selectedItem = pickerItems.find(i => i.value === value) || {}
+  const selectedItem = pickerItems.find((i) => i.value === value) || {}
   const title = selectedItem.label || defaultTitle
-  const selectedBaseItem = items.find(i => i[valueField] === value)
-  
-  const selectedItems = _.isArray(value) ? pickerItems.filter(i => value.includes(i.value)) : [selectedItem].filter(i => !_.isEmpty(i))
+  const selectedBaseItem = items.find((i) => i[valueField] === value)
+
+  const selectedItems = _.isArray(value) ? pickerItems.filter((i) => value.includes(i.value)) : [selectedItem].filter((i) => !_.isEmpty(i))
 
   return {pickerItems, selectedItems, title, selectedBaseItem, selectedItem, title}
 }
 
 export default usePicker
-
-
