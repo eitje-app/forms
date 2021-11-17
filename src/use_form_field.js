b24863f05114f06b946efa2c2da8eae14fb7919e
Abel van Hoek <abelvanhoek@gmail.com>
2021-11-17 13:13:25 +0100

    run prettier, ensure fn labels that return strings are also wrapped by 'eitje-label'

--- src/use_form_field.js
+++ src/use_form_field.js
@@ -1,14 +1,33 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, {useState, useEffect, useRef} from 'react'
 import {t, isScoped} from './base'
 import _ from 'lodash'
 import utils from '@eitje/utils'
 
-const allowEmptyString = (...vals) => vals.find(v => v == "" || v)
+const allowEmptyString = (...vals) => vals.find((v) => v == '' || v)
 
 function useFormField(props) {
-
-  let {required, disabled, error, disabledLabel, name, value, warning, labelStyle = {}, extraLabelStyle = {}, extraLabel, errorStyle = {color: 'red'},
-         defaultValue, labelVisible = true, formDisabled, formData, label, field, hidden, displayValue, warningStyle = {color: 'orange'} } = props
+  let {
+    required,
+    disabled,
+    error,
+    disabledLabel,
+    name,
+    value,
+    warning,
+    labelStyle = {},
+    extraLabelStyle = {},
+    extraLabel,
+    errorStyle = {color: 'red'},
+    defaultValue,
+    labelVisible = true,
+    formDisabled,
+    formData,
+    label,
+    field,
+    hidden,
+    displayValue,
+    warningStyle = {color: 'orange'},
+  } = props
   warning = utils.funcOrObj(warning, value, formData)
   const isDisabled = _.isFunction(disabled) ? disabled(formData) : disabled
   const isRequired = _.isFunction(required) ? required(formData) : required
@@ -16,37 +35,78 @@ function useFormField(props) {
   const actuallyDisabled = formDisabled || isDisabled
   const lbl = findLabel(props)
 
-  let finalLabel = !labelVisible ? null : isDisabled ? (disabledLabel || lbl) : lbl
+  let finalLabel = !labelVisible ? null : isDisabled ? disabledLabel || lbl : lbl
 
-  if(_.isString(finalLabel) ) {
-    finalLabel = <p className="eitje-label" style={labelStyle}> {utils.capitalize( t(finalLabel) ) } </p>
+  if (_.isString(finalLabel)) {
+    finalLabel = (
+      <p className="eitje-label" style={labelStyle}>
+        {utils.capitalize(t(finalLabel))}
+      </p>
+    )
   }
 
-  if(_.isString(extraLabel)) {
-    extraLabel = <p className="eitje-extra-label" style={extraLabelStyle}> {t(extraLabel)} </p>
+  if (_.isString(extraLabel)) {
+    extraLabel = (
+      <p className="eitje-extra-label" style={extraLabelStyle}>
+        {t(extraLabel)}
+      </p>
+    )
   }
-  if(_.isFunction(extraLabel)) extraLabel = extraLabel(props)
 
-  if(_.isFunction(finalLabel)) finalLabel = finalLabel(props)
+  if (_.isFunction(extraLabel)) {
+    extraLabel = extraLabel(props)
+    if (_.isString(extraLabel)) {
+      extraLabel = (
+        <p className="eitje-label" style={extraLabelStyle}>
+          {extraLabel}
+        </p>
+      )
+    }
+  }
 
-  warning = warning && <p className="warning-msg" style={warningStyle}> {warning} </p>
-  error = error && <p className="error-msg" style={errorStyle}> {error} </p>
+  if (_.isFunction(finalLabel)) {
+    finalLabel = finalLabel(props)
+    if (_.isString(finalLabel)) {
+      finalLabel = (
+        <p className="eitje-label" style={labelStyle}>
+          {finalLabel}
+        </p>
+      )
+    }
+  }
 
-  
-  return {required: isRequired, error, disabled: actuallyDisabled, label: finalLabel, extraLabel, warning, value: allowEmptyString(displayValue, value, defaultValue)}
+  warning = warning && (
+    <p className="warning-msg" style={warningStyle}>
+      {warning}
+    </p>
+  )
+  error = error && (
+    <p className="error-msg" style={errorStyle}>
+      {error}
+    </p>
+  )
+
+  return {
+    required: isRequired,
+    error,
+    disabled: actuallyDisabled,
+    label: finalLabel,
+    extraLabel,
+    warning,
+    value: allowEmptyString(displayValue, value, defaultValue),
+  }
 }
 
 const findLabel = ({label, name, field}) => {
-  if(label) return label;
+  if (label) return label
   const val = field
   const _name = 'field'
-  if(!val) return;
-  let finalValue = isScoped ? t(`form.${_name}.${val}`) : val;
-  finalValue = finalValue == `form.${_name}.${val}` ? val : finalValue 
+  if (!val) return
+  let finalValue = isScoped ? t(`form.${_name}.${val}`) : val
+  finalValue = finalValue == `form.${_name}.${val}` ? val : finalValue
   return finalValue
 }
 
 export default useFormField
 
-
-console.log("hiiii")
+console.log('hiiii')
