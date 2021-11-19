f11d3e68cf1cd0bff7704537e2b6418394ffdc86
Abel van Hoek <abelvanhoek@gmail.com>
2021-11-19 10:48:17 +0100

    use extraLabel from useFormField, not props

--- src/actions.js
+++ src/actions.js
@@ -3,77 +3,79 @@ import useFormField from './use_form_field'
 import {Button, Tooltip, t} from './base'
 import utils from '@eitje/utils'
 
-export const makeField = (Comp, {withLabel = true, withError = true, className = ""} = {}) => props => {
-  let {containerStyle = {}, field, Container = 'div', isTouched, disabledStyle = {opacity: 0.2}, 
-        containerProps = {}, submitStrategy, submitForm, value, 
-        LeftContainer = Fragment, RightContainer = Fragment, rightChildren, leftChildren, extraLabel,
-        leftContainerProps = {}, rightContainerProps = {}, readOnly, SubmitButton = Button,
-        isLayered = submitStrategy === 'inlineButton' || extraLabel, } = props
- 
- if(isLayered) {
-    LeftContainer = RightContainer = "div"
-    leftContainerProps = {className: 'form-container-left'}
-    rightContainerProps = {className: 'form-container-right'}
- }
+export const makeField =
+  (Comp, {withLabel = true, withError = true, className = ''} = {}) =>
+  (props) => {
+    let {
+      containerStyle = {},
+      field,
+      Container = 'div',
+      isTouched,
+      disabledStyle = {opacity: 0.2},
+      containerProps = {},
+      submitStrategy,
+      submitForm,
+      value,
+      LeftContainer = Fragment,
+      RightContainer = Fragment,
+      rightChildren,
+      leftChildren,
+      leftContainerProps = {},
+      rightContainerProps = {},
+      readOnly,
+      SubmitButton = Button,
+      extraLabel,
+      isLayered = submitStrategy === 'inlineButton' || extraLabel,
+    } = props
 
-  const isButtonSubmit = submitStrategy === 'inlineButton'
-    
-  const prupz = useFormField(props)
-  const {label, error, warning, disabled} = prupz
-  const classNames = [error && 'error-msg-container', 
-                      isLayered && 'form-container-split',
-                     readOnly && 'readOnly'].filter(
-      Boolean,
-  ).join(" ")
+    if (isLayered) {
+      LeftContainer = RightContainer = 'div'
+      leftContainerProps = {className: 'form-container-left'}
+      rightContainerProps = {className: 'form-container-right'}
+    }
 
+    const isButtonSubmit = submitStrategy === 'inlineButton'
 
-  let style = containerStyle
-  if(disabled) style = {...disabledStyle, ...style}
+    const prupz = useFormField(props)
+    let {label, error, warning, disabled} = prupz
+    extraLabel = prupz.extraLabel
+    const classNames = [error && 'error-msg-container', isLayered && 'form-container-split', readOnly && 'readOnly']
+      .filter(Boolean)
+      .join(' ')
 
-  const _Comp = <Comp innerClass={classNames} {...props} {...prupz}/>
-  return (
-      <Container className={`elementContainer ${classNames} ${className}`} style={style} {...containerProps} >
+    let style = containerStyle
+    if (disabled) style = {...disabledStyle, ...style}
 
+    const _Comp = <Comp innerClass={classNames} {...props} {...prupz} />
+    return (
+      <Container className={`elementContainer ${classNames} ${className}`} style={style} {...containerProps}>
         <LeftContainer {...leftContainerProps}>
           {renderLabel({...props, label, withLabel})}
           {extraLabel}
           {leftChildren}
           {!extraLabel && _Comp}
-          {withError && (error || warning) }
+          {withError && (error || warning)}
         </LeftContainer>
 
         <RightContainer {...rightContainerProps}>
           {extraLabel && _Comp}
           {rightChildren}
-          {isButtonSubmit && isTouched && !error &&
-            <div onClick={() => submitForm({field})} >
-              <SubmitButton> {t("submit")} </SubmitButton> 
+          {isButtonSubmit && isTouched && !error && (
+            <div onClick={() => submitForm({field})}>
+              <SubmitButton> {t('submit')} </SubmitButton>
             </div>
-          }
-          
-          </RightContainer>
-        
+          )}
+        </RightContainer>
       </Container>
-
-
-
     )
-}
+  }
 
-const renderError = ({error, warning}) => {
-
-}
-
+const renderError = ({error, warning}) => {}
 
 const renderLabel = ({label, withLabel, info}) => {
-  if(!withLabel || !label) return null;
-  if(info) {
-    return (
-      <Tooltip title={info}> 
-        {label} 
-      </Tooltip>
-      )
+  if (!withLabel || !label) return null
+  if (info) {
+    return <Tooltip title={info}>{label}</Tooltip>
   }
-  return label;
-
+  return label
 }
