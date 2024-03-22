d7a94b1b54b77c88134121b0a63574f0593fe106
Jurr <jschrofer@gmail.com>
2024-03-22 15:20:05 +0100

    fix styling

--- src/left_content.js
+++ src/left_content.js
@@ -1,87 +1,80 @@
-import React from "react";
-import utils from "@eitje/web_utils";
-import { t, config } from "./base";
-import { Text } from "@eitje/web_components";
-export const LeftContent = (props) => {
-	const { Comp } = props;
-	return (
-		<config.Layout direction="vertical" className="form-field-content-left">
-			<Label {...props} />
-			<Comp placeholder="..." {...props} />
-			<ValidationError {...props} />
-		</config.Layout>
-	);
-};
+import React from 'react'
+import utils from '@eitje/web_utils'
+import {t, config} from './base'
+import {Text} from '@eitje/web_components'
+export const LeftContent = props => {
+  const {Comp} = props
+  return (
+    <config.Layout direction="vertical" className="form-field-content-left">
+      <Label {...props} />
+      <Comp placeholder="..." {...props} />
+      <ValidationError {...props} />
+    </config.Layout>
+  )
+}
 
-const Label = (props) => {
-	const { required } = props;
-	const label = buildDecoration({ ...props, decorationType: "label" });
-	const extraLabel = buildDecoration({
-		...props,
-		decorationType: "extraLabel",
-	});
-	const popoutTitle = buildDecoration({ ...props, decorationType: "tooltip" });
-	return (
-		<>
-			<Text popoutTitle={popoutTitle} darkGrey fontSize={12}>
-				{label} {required && "*"}
-			</Text>
-			{extraLabel && (
-				<Text darkGrey fontSize={12}>
-					{extraLabel}
-				</Text>
-			)}
-		</>
-	);
-};
+const Label = props => {
+  const {required} = props
+  const label = buildDecoration({...props, decorationType: 'label'})
+  const extraLabel = buildDecoration({
+    ...props,
+    decorationType: 'extraLabel',
+  })
+  const popoutTitle = buildDecoration({...props, decorationType: 'tooltip'})
+  return (
+    <>
+      <Text truncate popoutTitle={popoutTitle} darkGrey fontSize={12}>
+        {label} {required && '*'}
+      </Text>
+      {extraLabel && (
+        <Text truncate darkGrey fontSize={12}>
+          {extraLabel}
+        </Text>
+      )}
+    </>
+  )
+}
 
-const ValidationError = ({ error }) => {
-	return (
-		<Text fontSize={12} mediumRed>
-			{error}
-		</Text>
-	);
-};
+const ValidationError = ({error}) => {
+  return (
+    <Text fontSize={12} mediumRed>
+      {error}
+    </Text>
+  )
+}
 
 const decorationDefaults = {
-	label: true,
-	placeholder: true,
-};
+  label: true,
+  placeholder: true,
+}
 
-const buildDecoration = (props) => {
-	const { decorationType } = props;
-	let val = props.hasOwnProperty(decorationType)
-		? props[decorationType]
-		: decorationDefaults[decorationType];
-	if (_.isFunction(val)) val = val(props);
-	if (_.isBoolean(val)) return makeTranslation(props);
-	return val;
-};
+const buildDecoration = props => {
+  const {decorationType} = props
+  let val = props.hasOwnProperty(decorationType) ? props[decorationType] : decorationDefaults[decorationType]
+  if (_.isFunction(val)) val = val(props)
+  if (_.isBoolean(val)) return makeTranslation(props)
+  return val
+}
 
-const numAtEndRegex = /-\d+\b/g; // this is done for compositeField, because it suffixes fields with -number, like user_id-1
+const numAtEndRegex = /-\d+\b/g // this is done for compositeField, because it suffixes fields with -number, like user_id-1
 
-const makeTranslation = (props) => {
-	let { label, decorationType, field, name } = props;
-	if (!field) return;
+const makeTranslation = props => {
+  let {label, decorationType, field, name} = props
+  if (!field) return
 
-	field = field.replace(numAtEndRegex, "");
+  field = field.replace(numAtEndRegex, '')
 
-	const namespaces = utils.alwaysDefinedArray(name);
-	const decorationName = utils.camelToSnake(decorationType);
-	const opts = props.i18nOpts || {};
+  const namespaces = utils.alwaysDefinedArray(name)
+  const decorationName = utils.camelToSnake(decorationType)
+  const opts = props.i18nOpts || {}
 
-	let allKeys = namespaces.map((n) => {
-		return `form.${n}.fields.${field}.${decorationName}`;
-	});
+  let allKeys = namespaces.map(n => {
+    return `form.${n}.fields.${field}.${decorationName}`
+  })
 
-	const lastOption = decorationType == "placeholder" && "...";
+  const lastOption = decorationType == 'placeholder' && '...'
 
-	allKeys = [
-		...allKeys,
-		`form.defaults.fields.${field}.${decorationType}`,
-		`records.default.fields.${field}`,
-		lastOption,
-	].filter(Boolean);
+  allKeys = [...allKeys, `form.defaults.fields.${field}.${decorationType}`, `records.default.fields.${field}`, lastOption].filter(Boolean)
 
-	return t(allKeys, opts); // form.exportLayouts.fields.name.label|placeholder|extraLabel|tooltip
-};
+  return t(allKeys, opts) // form.exportLayouts.fields.name.label|placeholder|extraLabel|tooltip
+}
