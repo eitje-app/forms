dc37661656e695544b92dec37ced0435095d19db
Jurr <jschrofer@gmail.com>
2024-03-22 14:03:20 +0100

    remove classnames

--- src/build_field.js
+++ src/build_field.js
@@ -1,112 +1,83 @@
-import React, { Fragment, useState, useRef } from "react";
-import useFormField from "./use_form_field";
-import {
-	Button,
-	Tooltip,
-	config,
-	t,
-	tooltipElement,
-	defaultIcon,
-	clearIcon,
-} from "./base";
-import utils from "@eitje/utils";
-import {
-	makeRegisteredField,
-	makeNewRegisteredField,
-} from "./use_register_field";
-import { findFns } from "./field_click_helper";
-import { LeftContent } from "./left_content";
-import { RightContent } from "./right_content";
+import React, {Fragment, useState, useRef} from 'react'
+import useFormField from './use_form_field'
+import {Button, Tooltip, config, t, tooltipElement, defaultIcon, clearIcon} from './base'
+import utils from '@eitje/utils'
+import {makeRegisteredField, makeNewRegisteredField} from './use_register_field'
+import {findFns} from './field_click_helper'
+import {LeftContent} from './left_content'
+import {RightContent} from './right_content'
 
 const decorateField =
-	(Comp, compOpts = {}) =>
-	(props) => {
-		let {
-			field,
-			value,
-			readOnly,
-			formData,
-			className,
-			error,
-			disabled,
-			clearIcon,
-			icon = defaultIcon,
-		} = props;
+  (Comp, compOpts = {}) =>
+  props => {
+    let {field, value, readOnly, formData, className, error, disabled, clearIcon, icon = defaultIcon} = props
 
-		const [fieldOpen, setOpen] = useState(false);
+    const [fieldOpen, setOpen] = useState(false)
 
-		const onOpenChange = (open) => {
-			// this is for date&timepicker, if you click outside antd's area, they trigger a 'clickOutside' event and hide the dropdown menu.
-			// sadly, this event is fired BEFORE our click, and thus we never know if the dropdown ACTUALLY came from a closed state, or if it was just closed by the clickOutside event
-			// the 200ms here is a proxy to differentiate between 'actually closed' & 'just closed by event trigger'
-			setTimeout(() => {
-				setOpen(open);
-			}, 200);
-		};
+    const onOpenChange = open => {
+      // this is for date&timepicker, if you click outside antd's area, they trigger a 'clickOutside' event and hide the dropdown menu.
+      // sadly, this event is fired BEFORE our click, and thus we never know if the dropdown ACTUALLY came from a closed state, or if it was just closed by the clickOutside event
+      // the 200ms here is a proxy to differentiate between 'actually closed' & 'just closed by event trigger'
+      setTimeout(() => {
+        setOpen(open)
+      }, 200)
+    }
 
-		const clickChild = (e) => {
-			const classNames = opts.className.split(" ");
-			const fn = findFns[classNames.find((name) => findFns[name])];
-			fn && fn(element.current, { open: fieldOpen });
-		};
+    const clickChild = e => {
+      const classNames = opts.className.split(' ')
+      const fn = findFns[classNames.find(name => findFns[name])]
+      fn && fn(element.current, {open: fieldOpen})
+    }
 
-		const required = utils.funcOrVal(props.required, formData);
+    const required = utils.funcOrVal(props.required, formData)
 
-		const opts = utils.funcOrVal(compOpts, props);
-		const allProps = {
-			...opts,
-			...props,
-			required,
-			Comp,
-			onOpenChange,
-			onVisibleChange: onOpenChange,
-		};
+    const opts = utils.funcOrVal(compOpts, props)
+    const allProps = {
+      ...opts,
+      ...props,
+      required,
+      Comp,
+      onOpenChange,
+      onVisibleChange: onOpenChange,
+    }
 
-		const classNames = utils.makeCns(
-			`eitje-form-3-field`,
-			`eitje-form-3-field-${field}`,
-			disabled && "eitje-form-3-field-disabled",
-			error && "eitje-form-3-field-error",
-			readOnly && "eitje-form-3-read-only",
-			icon && "eitje-form-3-field-has-icon",
-			clearIcon && "eitje-form-3-field-has-clear-icon",
-			className,
-			opts.className,
-		);
+    const classNames = utils.makeCns(
+      `eitje-form-3-field`,
+      disabled && 'disabled',
+      error && 'error',
+      readOnly && 'read-only',
+      className,
+      opts.className,
+    )
 
-		const element = useRef(null);
-		const { onChange, ...propsWithoutChange } = allProps;
-		return (
-			<div
-				{...propsWithoutChange}
-				ref={element}
-				onClick={clickChild}
-				className={classNames}
-				height="full"
-				padding="16 24"
-				horizontal="spaceBetween"
-			>
-				<TooltipWrapper {...allProps}>
-					<config.Layout
-						className="form-field-content"
-						horizontal="spaceBetween"
-						height="full"
-						padding="16 24"
-					>
-						<LeftContent {...allProps} />
-						<RightContent {...allProps} />
-					</config.Layout>
-				</TooltipWrapper>
-			</div>
-		);
-	};
+    const element = useRef(null)
+    const {onChange, ...propsWithoutChange} = allProps
+    return (
+      <div
+        {...propsWithoutChange}
+        ref={element}
+        onClick={clickChild}
+        className={classNames}
+        height="full"
+        padding="16 24"
+        horizontal="spaceBetween"
+      >
+        <TooltipWrapper {...allProps}>
+          <config.Layout className="form-field-content" horizontal="spaceBetween" height="full" padding="16 24">
+            <LeftContent {...allProps} />
+            <RightContent {...allProps} />
+          </config.Layout>
+        </TooltipWrapper>
+      </div>
+    )
+  }
 
-const TooltipWrapper = ({ tooltip, children }) => {
-	if (!tooltip) return children;
-	return <Tooltip> {children} </Tooltip>;
-};
+const TooltipWrapper = ({tooltip, children}) => {
+  if (!tooltip) return children
+  return <Tooltip> {children} </Tooltip>
+}
 
 export const makeNewField = (Comp, compOpts = {}) => {
-	const Formified = decorateField(Comp, compOpts);
-	return makeNewRegisteredField(Formified, compOpts);
-};
+  const Formified = decorateField(Comp, compOpts)
+  return makeNewRegisteredField(Formified, compOpts)
+}
--- src/right_content.js
+++ src/right_content.js
@@ -1,57 +1,36 @@
-import React from "react";
-import {
-	Button,
-	Tooltip,
-	t,
-	config,
-	tooltipElement,
-	defaultIcon,
-	clearIcon as clearIconImg,
-} from "./base";
-import { Text, Icon } from "@eitje/web_components";
+import React from 'react'
+import {Button, Tooltip, t, config, tooltipElement, defaultIcon, clearIcon as clearIconImg} from './base'
+import {Text, Icon} from '@eitje/web_components'
 
-export const RightContent = (props) => {
-	const { icon = defaultIcon, onChange, clearIcon, defaultPickerValue } = props;
-	const hide = !icon && !clearIcon;
-	const rightElement = getRightElement(props);
-	if (hide) return;
+export const RightContent = props => {
+	const {icon = defaultIcon, onChange, clearIcon, defaultPickerValue} = props
+	const hide = !icon && !clearIcon
+	const rightElement = getRightElement(props)
+	if (hide) return
 	return (
 		<config.Layout className="form-field-content-right">
-			{icon && <Icon name="caret-down" size={14} />}
-			{clearIcon && (
-				<Icon
-					name="cross"
-					onClick={() => onChange(defaultPickerValue)}
-					size={14}
-				/>
-			)}
 			{rightElement}
+			{clearIcon && <Icon name="cross" onClick={() => onChange(defaultPickerValue)} size={12} />}
+			{!clearIcon && icon && <Icon name="caret-down" size={12} />}
 		</config.Layout>
-	);
-};
+	)
+}
 
-const getRightElement = ({ rightElement, ...rest }) => {
-	if (!_.isString(rightElement)) return rightElement;
-	if (rightElement == "charCounter") {
-		return <CharCounter {...rest} />;
+const getRightElement = ({rightElement, ...rest}) => {
+	if (!_.isString(rightElement)) return rightElement
+	if (rightElement == 'charCounter') {
+		return <CharCounter {...rest} />
 	}
-};
+}
 
-const makeCharClass = (
-	charsLeft,
-	{ warningThreshold = 10, dangerThreshold = 5 },
-) => {
-	if (charsLeft < dangerThreshold) return "eitje-form-3-char-counter-danger";
-	if (charsLeft < warningThreshold) return "eitje-form-3-char-counter-warning";
-	return "";
-};
+const makeCharClass = (charsLeft, {warningThreshold = 10, dangerThreshold = 5}) => {
+	if (charsLeft < dangerThreshold) return 'eitje-form-3-char-counter-danger'
+	if (charsLeft < warningThreshold) return 'eitje-form-3-char-counter-warning'
+	return ''
+}
 
-const CharCounter = ({ maxLength, value, ...rest }) => {
-	const charsLeft = maxLength - (value?.length || 0);
-	const className = makeCharClass(charsLeft, rest);
-	return (
-		<Text className={`eitje-form-3-char-counter ${className}`}>
-			{charsLeft}
-		</Text>
-	);
-};
+const CharCounter = ({maxLength, value, ...rest}) => {
+	const charsLeft = maxLength - (value?.length || 0)
+	const className = makeCharClass(charsLeft, rest)
+	return <Text className={`eitje-form-3-char-counter ${className}`}>{charsLeft}</Text>
+}
