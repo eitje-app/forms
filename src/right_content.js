7528bd978b89b7f15d42c07cfa30ce25a10b0ad4
Jurr <jschrofer@gmail.com>
2024-03-24 15:18:34 +0100

    hide cross when showonly or disabled + simplify HTML

--- src/right_content.js
+++ src/right_content.js
@@ -3,14 +3,15 @@ import {Button, Tooltip, t, config, tooltipElement, defaultIcon, clearIcon as cl
 import {Text, Icon} from '@eitje/web_components'
 
 export const RightContent = props => {
-	const {icon = defaultIcon, onChange, clearIcon, defaultPickerValue} = props
+	const {icon = defaultIcon, onChange, clearIcon, defaultPickerValue, disabled, readOnly} = props
 	const hide = !icon && !clearIcon
 	const rightElement = getRightElement(props)
+
 	if (hide) return
 	return (
 		<config.Layout className="form-field-content-right">
 			{rightElement}
-			{clearIcon && <Icon name="cross" onClick={() => onChange(defaultPickerValue)} size={12} />}
+			{clearIcon && !disabled && !readOnly && <Icon name="cross" onClick={() => onChange(defaultPickerValue)} size={12} />}
 			{!clearIcon && icon && <Icon name="caret-down" size={12} />}
 		</config.Layout>
 	)
@@ -23,14 +24,7 @@ const getRightElement = ({rightElement, ...rest}) => {
 	}
 }
 
-const makeCharClass = (charsLeft, {warningThreshold = 10, dangerThreshold = 5}) => {
-	if (charsLeft < dangerThreshold) return 'eitje-form-3-char-counter-danger'
-	if (charsLeft < warningThreshold) return 'eitje-form-3-char-counter-warning'
-	return ''
-}
-
 const CharCounter = ({maxLength, value, ...rest}) => {
 	const charsLeft = maxLength - (value?.length || 0)
-	const className = makeCharClass(charsLeft, rest)
-	return <Text className={`eitje-form-3-char-counter ${className}`}>{charsLeft}</Text>
+	return <Text>{charsLeft}</Text>
 }
