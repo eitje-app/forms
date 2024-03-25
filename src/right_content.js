27571026fa4038aa7cd9ca86831490c9b968b7bf
Abel van Hoek <abel@eitje.app>
2024-03-25 15:58:25 +0100

    different icon strategy: ensure only one icon can be rendered simultaneously

--- src/right_content.js
+++ src/right_content.js
@@ -1,32 +1,39 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import {Button, Tooltip, t, config, tooltipElement, defaultIcon, clearIcon as clearIconImg} from './base'
-import {Text, Icon} from '@eitje/web_components'
+import {Text, Icon, PopoutCard} from '@eitje/web_components'
 
 export const RightContent = props => {
-	const {icon = defaultIcon, onChange, clearIcon, defaultPickerValue, disabled, readOnly} = props
-	const hide = !icon && !clearIcon
-	const rightElement = getRightElement(props)
+  const rightElement = getRightElement(props)
+  const iconProps = getIcon(props)
+  if (!iconProps && !rightElement) return null
+  const {Wrapper = Fragment, wrapperProps, ...iconRest} = iconProps || {}
+  return (
+    <config.Layout className="form-field-content-right">
+      {rightElement}
+      {iconProps && (
+        <Wrapper {...wrapperProps}>
+          <Icon size={12} {...iconRest} />
+        </Wrapper>
+      )}
+    </config.Layout>
+  )
+}
 
-	if (hide) return
-	return (
-		<config.Layout className="form-field-content-right">
-			{rightElement}
-			{clearIcon && !disabled && !readOnly && (
-				<Icon name="cross" className="cross-icon" onClick={() => onChange(defaultPickerValue)} size={12} />
-			)}
-			{!clearIcon && icon && <Icon name="caret-down" size={12} />}
-		</config.Layout>
-	)
+const getIcon = ({readOnly, disabled, value, onChange, icon, defaultPickerValue, clearIcon = true}) => {
+  if (disabled) return
+  if (readOnly) return {name: 'locked', Wrapper: PopoutCard, wrapperProps: {title: t('form.general.tooltips.read_only')}}
+  if (clearIcon && value) return {name: 'cross', className: 'cross-icon', onClick: () => onChange(defaultPickerValue)}
+  if (icon) return {name: 'caret-down'}
 }
 
 const getRightElement = ({rightElement, ...rest}) => {
-	if (!_.isString(rightElement)) return rightElement
-	if (rightElement == 'charCounter') {
-		return <CharCounter {...rest} />
-	}
+  if (!_.isString(rightElement)) return rightElement
+  if (rightElement == 'charCounter') {
+    return <CharCounter {...rest} />
+  }
 }
 
 const CharCounter = ({maxLength, value, ...rest}) => {
-	const charsLeft = maxLength - (value?.length || 0)
-	return <Text>{charsLeft}</Text>
+  const charsLeft = maxLength - (value?.length || 0)
+  return <Text>{charsLeft}</Text>
 }
