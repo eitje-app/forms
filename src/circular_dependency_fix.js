fd2740e33d67693a767b122e2481cef7d7a57498
Abel van Hoek <abel@eitje.app>
2024-05-29 16:33:40 +0200

    new strategy: react.lazy

--- src/circular_dependency_fix.js
+++ src/circular_dependency_fix.js
@@ -1,21 +1,15 @@
-const cache = {}
+import React, {lazy, Suspense} from 'react'
 
-export const getDynamicComponent = async (moduleName, componentName) => {
-  if (!cache[componentName]) {
-    const module = await import(moduleName)
-    cache[componentName] = module[componentName]
-  }
-  return cache[componentName]
+const loadable = importFunc => {
+  const LazyComponent = lazy(importFunc)
+
+  return props => (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LazyComponent {...props} />
+    </Suspense>
+  )
 }
 
-// Immediate export using a promise resolution pattern
-export const createComponent = (moduleName, componentName) => {
-  return async props => {
-    const Component = await getDynamicComponent(moduleName, componentName)
-    return <Component {...props} />
-  }
-}
-
-export const Text = createComponent('@eitje/web_components', 'Text')
-export const Icon = createComponent('@eitje/web_components', 'Icon')
-export const PopoutCard = createComponent('@eitje/web_components', 'PopoutCard')
+export const Text = loadable(() => import('@eitje/web_components').then(module => ({default: module.Text})))
+export const Icon = loadable(() => import('@eitje/web_components').then(module => ({default: module.Icon})))
+export const PopoutCard = loadable(() => import('@eitje/web_components').then(module => ({default: module.PopoutCard})))
