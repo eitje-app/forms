216eb2c669e4f359a35ddf837942aaff3c01212a
Abel <abel@eitje.app>
2024-12-04 11:56:01 +0100

    auto set charcounter if we have maxLength as prop

--- src/right_content.js
+++ src/right_content.js
@@ -1,74 +1,62 @@
-import React, { Fragment } from "react";
-import {
-  Button,
-  Tooltip,
-  t,
-  config,
-  tooltipElement,
-  defaultIcon,
-  clearIcon as clearIconImg,
-} from "./base";
-import { Text, Icon, PopoutCard } from "./circular_dependency_fix";
-import utils from "@eitje/web_utils";
+import React, {Fragment} from 'react'
+import {Button, Tooltip, t, config, tooltipElement, defaultIcon, clearIcon as clearIconImg} from './base'
+import {Text, Icon, PopoutCard} from './circular_dependency_fix'
+import utils from '@eitje/web_utils'
 
-export const RightContent = (props) => {
-  const rightElement = getRightElement(props);
-  const icons = utils.alwaysArray(getIcon(props));
-  if (!utils.exists(icons) && !rightElement) return null;
+export const RightContent = props => {
+  const rightElement = getRightElement(props)
+  const icons = utils.alwaysArray(getIcon(props))
+  if (!utils.exists(icons) && !rightElement) return null
   return (
     <config.Layout className="form-field-content-right">
       {rightElement}
-      {icons.map((i) => (
+      {icons.map(i => (
         <FormIcon {...i} />
       ))}
     </config.Layout>
-  );
-};
+  )
+}
 
-const FormIcon = ({ Wrapper = Fragment, wrapperProps, ...rest }) => {
+const FormIcon = ({Wrapper = Fragment, wrapperProps, ...rest}) => {
   return (
     <Wrapper {...wrapperProps}>
       <Icon size={12} {...rest} />
     </Wrapper>
-  );
-};
+  )
+}
 
-const getIcon = ({
-  readOnly,
-  disabled,
-  value,
-  required,
-  onChange,
-  icon,
-  defaultPickerValue,
-  clearIcon = true,
-}) => {
-  if (disabled) return;
+const getIcon = ({readOnly, disabled, value, required, onChange, icon, defaultPickerValue, clearIcon = true}) => {
+  if (disabled) return
   if (readOnly)
     return {
-      name: "locked",
+      name: 'locked',
       Wrapper: PopoutCard,
-      wrapperProps: { title: t("form.general.tooltips.read_only") },
-    };
-  let buttons = [];
+      wrapperProps: {title: t('form.general.tooltips.read_only')},
+    }
+  let buttons = []
   if (clearIcon && value && !required)
     buttons.push({
-      name: "cross",
-      className: "cross-icon",
+      name: 'cross',
+      className: 'cross-icon',
       onClick: () => onChange(defaultPickerValue),
-    });
-  if (icon) buttons.push({ name: "caret-down" });
-  return buttons;
-};
+    })
+  if (icon) buttons.push({name: 'caret-down'})
+  return buttons
+}
 
-const getRightElement = ({ rightElement, ...rest }) => {
-  if (!_.isString(rightElement)) return rightElement;
-  if (rightElement == "charCounter") {
-    return <CharCounter {...rest} />;
+const getRightElement = ({rightElement, ...rest}) => {
+  if (utils.exists(rest.maxLength) && !rightElement) rightElement = 'charCounter'
+
+  if (!_.isString(rightElement)) {
+    return rightElement
   }
-};
 
-const CharCounter = ({ maxLength, value, ...rest }) => {
-  const charsLeft = maxLength - (value?.length || 0);
-  return <Text>{charsLeft}</Text>;
-};
+  if (rightElement == 'charCounter') {
+    return <CharCounter {...rest} />
+  }
+}
+
+const CharCounter = ({maxLength, value, ...rest}) => {
+  const charsLeft = maxLength - (value?.length || 0)
+  return <Text>{charsLeft}</Text>
+}
