174d11d1fa9ebe95120c657d80df3f063c8c9711
Abel <abel@eitje.app>
2025-03-11 12:06:39 +0100

    add [field] watcher to useEffect to prevent strange bugs if keys aren't present

--- src/use_register_field.js
+++ src/use_register_field.js
@@ -6,21 +6,21 @@ import {useForm} from './context_form'
 import useFormField from './use_form_field'
 
 export const useRegisterField = (field, fieldProps) => {
-	const {registerField = _.noop, enhanceField = _.noop, unregisterField = _.noop} = useForm()
-	useEffect(() => {
-		registerField(field, fieldProps)
-		return () => unregisterField(field)
-	}, [])
-	return enhanceField(field, fieldProps)
+  const {registerField = _.noop, enhanceField = _.noop, unregisterField = _.noop} = useForm()
+  useEffect(() => {
+    registerField(field, fieldProps)
+    return () => unregisterField(field)
+  }, [field])
+  return enhanceField(field, fieldProps)
 }
 
 export const makeRegisteredField = Comp => props => {
-	const formProps = useRegisterField(props.field, props)
-	const formFieldProps = useFormField(formProps)
-	return <Comp {...props} {...formProps} {...formFieldProps} form />
+  const formProps = useRegisterField(props.field, props)
+  const formFieldProps = useFormField(formProps)
+  return <Comp {...props} {...formProps} {...formFieldProps} form />
 }
 
 export const makeNewRegisteredField = Comp => props => {
-	const formProps = useRegisterField(props.field, props)
-	return <Comp {...props} {...formProps} form />
+  const formProps = useRegisterField(props.field, props)
+  return <Comp {...props} {...formProps} form />
 }
